9ad2d4c444d61642e52cf50adc1d869f35b09b61
tuurlijk <tuurlijk@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-03 14:47:22 +0000

    - Add task for cleaning of hidden entries - Rename tasks - Remove the term 'truncate' from the extension - Update manual
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tablecleaner/trunk@76120 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "tablecleaner".
  *
- * Auto generated 14-03-2013 20:27
+ * Auto generated 14-03-2013 20:55
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -32,18 +32,20 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'Donation Based Hosting',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' =>
+	array(
+		'depends' =>
+		array(
 			'typo3' => '4.5.0-0.0.0',
 		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
+		'conflicts' =>
+		array(),
+		'suggests' =>
+		array(),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:16:"ext_autoload.php";s:4:"3886";s:12:"ext_icon.gif";s:4:"62c8";s:17:"ext_localconf.php";s:4:"f573";s:14:"ext_tables.php";s:4:"8a8d";s:23:"Classes/Tasks/Clean.php";s:4:"3031";s:46:"Classes/Tasks/CleanAdditionalFieldProvider.php";s:4:"ebee";s:26:"Classes/Tasks/Truncate.php";s:4:"54ec";s:49:"Classes/Tasks/TruncateAdditionalFieldProvider.php";s:4:"0f86";s:51:"Resources/Private/Language/ContextSensitiveHelp.xml";s:4:"b172";s:40:"Resources/Private/Language/locallang.xml";s:4:"2d25";s:14:"doc/manual.sxw";s:4:"d41d";}',
-	'suggests' => array(
-	),
+	'_md5_values_when_last_written' => 'a:11:{s:16:"ext_autoload.php";s:4:"3886";s:12:"ext_icon.gif";s:4:"62c8";s:17:"ext_localconf.php";s:4:"f573";s:14:"ext_tables.php";s:4:"8a8d";s:23:"Classes/Tasks/Deleted.php";s:4:"3031";s:46:"Classes/Tasks/CleanAdditionalFieldProvider.php";s:4:"ebee";s:26:"Classes/Tasks/Truncate.php";s:4:"54ec";s:49:"Classes/Tasks/TruncateAdditionalFieldProvider.php";s:4:"0f86";s:51:"Resources/Private/Language/ContextSensitiveHelp.xml";s:4:"b172";s:40:"Resources/Private/Language/locallang.xml";s:4:"2d25";s:14:"doc/manual.sxw";s:4:"d41d";}',
+	'suggests' =>
+	array(),
 );
 
 ?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -5,18 +5,25 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_tablecleaner_tasks_Clean'] = array(
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_tablecleaner_tasks_Deleted'] = array(
 	'extension'        => 'tablecleaner',
-	'title'            => 'LLL:EXT:tablecleaner/Resources/Private/Language/locallang.xml:tasks.clean.title',
-	'description'      => 'LLL:EXT:tablecleaner/Resources/Private/Language/locallang.xml:tasks.clean.description',
-	'additionalFields' => 'tx_tablecleaner_tasks_CleanAdditionalFieldProvider'
+	'title'            => 'LLL:EXT:tablecleaner/Resources/Private/Language/locallang.xml:tasks.deleted.title',
+	'description'      => 'LLL:EXT:tablecleaner/Resources/Private/Language/locallang.xml:tasks.deleted.description',
+	'additionalFields' => 'tx_tablecleaner_tasks_DeletedAdditionalFieldProvider'
 );
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_tablecleaner_tasks_Truncate'] = array(
-	'extension'        => 'tabletruncater',
-	'title'            => 'LLL:EXT:tablecleaner/Resources/Private/Language/locallang.xml:tasks.truncate.title',
-	'description'      => 'LLL:EXT:tablecleaner/Resources/Private/Language/locallang.xml:tasks.truncate.description',
-	'additionalFields' => 'tx_tablecleaner_tasks_TruncateAdditionalFieldProvider'
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_tablecleaner_tasks_Hidden'] = array(
+	'extension'        => 'tablecleaner',
+	'title'            => 'LLL:EXT:tablecleaner/Resources/Private/Language/locallang.xml:tasks.hidden.title',
+	'description'      => 'LLL:EXT:tablecleaner/Resources/Private/Language/locallang.xml:tasks.hidden.description',
+	'additionalFields' => 'tx_tablecleaner_tasks_HiddenAdditionalFieldProvider'
+);
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_tablecleaner_tasks_Expired'] = array(
+	'extension'        => 'tablecleaner',
+	'title'            => 'LLL:EXT:tablecleaner/Resources/Private/Language/locallang.xml:tasks.expired.title',
+	'description'      => 'LLL:EXT:tablecleaner/Resources/Private/Language/locallang.xml:tasks.expired.description',
+	'additionalFields' => 'tx_tablecleaner_tasks_ExpiredAdditionalFieldProvider'
 );
 
 ?>
