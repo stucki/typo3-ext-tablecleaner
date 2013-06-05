67cb3738d79e987fb2155e4e4c1c5947acb2675a
tuurlijk <tuurlijk@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-05 07:48:17 +0000

    - Update manual - Update ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tablecleaner/trunk@76217 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "tablecleaner".
  *
- * Auto generated 14-03-2013 20:55
+ * Auto generated 05-06-2013 07:48
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -32,20 +32,18 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'Donation Based Hosting',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'constraints' =>
-	array(
-		'depends' =>
-		array(
+	'constraints' => array(
+		'depends' => array(
 			'typo3' => '4.5.0-0.0.0',
 		),
-		'conflicts' =>
-		array(),
-		'suggests' =>
-		array(),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:16:"ext_autoload.php";s:4:"3886";s:12:"ext_icon.gif";s:4:"62c8";s:17:"ext_localconf.php";s:4:"f573";s:14:"ext_tables.php";s:4:"8a8d";s:23:"Classes/Tasks/Deleted.php";s:4:"3031";s:46:"Classes/Tasks/CleanAdditionalFieldProvider.php";s:4:"ebee";s:26:"Classes/Tasks/Truncate.php";s:4:"54ec";s:49:"Classes/Tasks/TruncateAdditionalFieldProvider.php";s:4:"0f86";s:51:"Resources/Private/Language/ContextSensitiveHelp.xml";s:4:"b172";s:40:"Resources/Private/Language/locallang.xml";s:4:"2d25";s:14:"doc/manual.sxw";s:4:"d41d";}',
-	'suggests' =>
-	array(),
+	'_md5_values_when_last_written' => 'a:13:{s:16:"ext_autoload.php";s:4:"c9f6";s:12:"ext_icon.gif";s:4:"62c8";s:17:"ext_localconf.php";s:4:"b130";s:14:"ext_tables.php";s:4:"8a8d";s:25:"Classes/Tasks/Deleted.php";s:4:"6c98";s:48:"Classes/Tasks/DeletedAdditionalFieldProvider.php";s:4:"07d0";s:25:"Classes/Tasks/Expired.php";s:4:"03e6";s:48:"Classes/Tasks/ExpiredAdditionalFieldProvider.php";s:4:"d802";s:24:"Classes/Tasks/Hidden.php";s:4:"5b6a";s:47:"Classes/Tasks/HiddenAdditionalFieldProvider.php";s:4:"33b5";s:51:"Resources/Private/Language/ContextSensitiveHelp.xml";s:4:"4f33";s:40:"Resources/Private/Language/locallang.xml";s:4:"5da6";s:14:"doc/manual.sxw";s:4:"d41d";}',
+	'suggests' => array(
+	),
 );
 
 ?>
